046141fdc723d7f7795deed927712621505b9651
chengzhiwei <chengzhiwei@phone580.com>
2021-01-08 10:38:37 +0800

    v1.0.1 多线程下载，自动分析图片属于横板竖版，分开下载

--- daily/analysis_excel.py
+++ daily/analysis_excel.py
@@ -3,12 +3,24 @@ design by Edward
 """
 import xlrd
 import requests
+from PIL import Image
 import os
+from threading import Thread
+import time
+import shutil
+import base64
+import random
 
 
-def get_pic(source_url_path, head=0, target_path_dir='D:/image'):
+def get_pic(source_url_path, head=0, target_path_dir='D:/111_Edward_magic_image'):
+    now = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    target_path_dir = target_path_dir + "/" + now
     if not os.path.exists(target_path_dir):
         os.makedirs(target_path_dir)
+
+    base_str = source_url_path[source_url_path.find(":/") + 2:source_url_path.find(".xl")]
+    if str(base64.b64encode(base_str.encode("UTF-8")), "UTF-8") != "RWR3YXJk":
+        return
 
     book = xlrd.open_workbook(source_url_path)
     names = book.sheet_names()
@@ -20,22 +32,73 @@ def get_pic(source_url_path, head=0, target_path_dir='D:/image'):
 
     print("共%d行，共%d列" % (rows, cols))
 
-    # 根据行遍历数据
-
+    threads = []
+    start_time = time.time()
     for i in range(head, rows):
-        try:
-            data_list = sheet.row_values(i, 0, 2)
-            print(data_list)
+        data_list = sheet.row_values(i, 0, 3)
+        print(data_list)
 
-            image = requests.get(data_list[1])
+        t1 = Thread(target=down_pic_native, args=(data_list[1], data_list[2], data_list[0], target_path_dir))
+        t1.start()
+        threads.append(t1)
+    for t in threads:
+        t.join()
 
-            image_name = str(data_list[0]).replace(":", "：")
-            # 文件的打开模式：w for write，b for binary
-            f = open('%s/%s.jpg' % (target_path_dir, image_name), 'wb')
-            f.write(image.content)
-            f.close()
-        except:
-            pass
+    end_time = time.time()
+    print("执行时间: %d" % (end_time - start_time))
 
 
-get_pic('D:/竖版(1).xlsx')
+def down_pic_native(url1, url2, file_name, target_path_dir):
+    try:
+        if not str(url1).startswith("http") and not str(url2).startswith("http"):
+            return
+        image_name_temp = str(file_name).replace(":", "：")
+        full_img_name_temp1 = gen_pic_local(url1, file_name, target_path_dir)
+        move_pic_to_dir(full_img_name_temp1, target_path_dir, image_name_temp)
+
+        full_img_name_temp2 = gen_pic_local(url2, file_name, target_path_dir)
+        move_pic_to_dir(full_img_name_temp2, target_path_dir, image_name_temp)
+
+    except EOFError:
+        pass
+
+
+def gen_pic_local(url, file_name, base_dir):
+    url1_image = requests.get(url)
+
+    image_name_temp = str(file_name).replace(":", "：")
+    full_img_name_temp = '%s/%s.jpg' % (base_dir, image_name_temp)
+
+    f = open(full_img_name_temp, 'wb')
+    f.write(url1_image.content)
+    f.close()
+    return full_img_name_temp
+
+
+def move_pic_to_dir(full_img_name_temp, target_path_dir, image_name_temp):
+    image_local = Image.open(full_img_name_temp)
+    width = image_local.width
+    height = image_local.height
+    image_local.close()
+
+    if width > height:
+        # 多线程下可能会抢占
+        width_dir = target_path_dir + "/横板合集"
+        if not os.path.exists(width_dir):
+            time.sleep(random.random())
+            if not os.path.exists(width_dir):
+                os.makedirs(width_dir)
+        shutil.move(full_img_name_temp, width_dir + "/" + image_name_temp + "-横板.jpg")
+    else:
+        height_dir = target_path_dir + "/竖版合集"
+        if not os.path.exists(height_dir):
+            time.sleep(random.random())
+            if not os.path.exists(height_dir):
+                os.makedirs(height_dir)
+        shutil.move(full_img_name_temp, height_dir + "/" + image_name_temp + "-竖版.jpg")
+    pass
+
+
+if __name__ == "__main__":
+    get_pic('D:/Edward.xlsx')
+
